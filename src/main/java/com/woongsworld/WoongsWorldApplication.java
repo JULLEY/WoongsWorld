5837e8d3be3088c618174e836501732ee4207b01
hwchoi <hwchoi@lsitc.com>
2024-02-13 10:29:06 +0900

    Initial commit

---
+++ src/main/java/com/woongsworld/WoongsWorldApplication.java
@@ -0,0 +1,13 @@
+package com.woongsworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoongsWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoongsWorldApplication.class, args);
+	}
+
+}
